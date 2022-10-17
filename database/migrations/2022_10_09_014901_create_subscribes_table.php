94cd31ff0618a1345d72a9d9e7e11894135c843b
YuSa0-6 <fuku.sugar@icloud.com>
2022-10-17 11:38:21 +0900

    add culum endPayment

--- database/migrations/2022_10_09_014901_create_subscribes_table.php
+++ database/migrations/2022_10_09_014901_create_subscribes_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('cycle');
             $table->integer('price');
             $table->date('firstPaymentDate')->nullable();
+            $table->date('endPaymentDate')->nullable();
             $table->string('payment');
             $table->timestamps();
         });
