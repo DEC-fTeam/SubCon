6a69a2b8eb8173f854d6ab3e199b3c0afe455a4f
soratooo <0116sorato@icloud.com>
2022-10-22 16:26:02 +0900

    不要カラム

---
+++ database/migrations/2022_10_22_071814_drop_column_first_payment_date_andend_payment_date_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscribes', function (Blueprint $table) {
+             $table->dropColumn('firstPaymentDate');
+             $table->dropColumn('endPaymentDate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscribes', function (Blueprint $table) {
+            //
+        });
+    }
+};
