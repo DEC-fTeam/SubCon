5843a04bbc1605f171a0a3f51cf2c2fe42564141
soratooo <0116sorato@icloud.com>
2022-10-22 16:32:43 +0900

    カラムの削除に伴う修正

--- resources/views/subscribe/show.blade.php
+++ resources/views/subscribe/show.blade.php
@@ -36,12 +36,6 @@
                 {{$subscribe->payment}}
               </p>
             </div>
-            <div class="flex flex-col mb-4">
-              <p class="mb-2 uppercase font-bold text-lg text-grey-darkest">Start payDay</p>
-              <p class="py-2 px-3 text-grey-darkest" id="firstpayment">
-                {{$subscribe->firstpayment}}
-              </p>
-            </div>
             <a href="{{ route('subscribe.delete',$subscribe->id) }}">
               <h3 class="text-left font-bold text-lg text-grey-dark">削除</h3>
             </a>
